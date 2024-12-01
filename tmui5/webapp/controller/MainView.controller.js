0ac0cd358530f76f00b3c005cb9cf1e96a712499
alikapllan <alihkaplan44@gmail.com>
2024-12-01 15:59:48 +0100

    Implement Generic Tiles on Main Page

--- tmui5/webapp/controller/MainView.controller.js
+++ tmui5/webapp/controller/MainView.controller.js
@@ -1,15 +1,21 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/model/json/JSONModel"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, JSONModel) {
+    "use strict";
 
-        return Controller.extend("tmui5.controller.MainView", {
-            onInit: function () {
+    return Controller.extend("tmui5.controller.MainView", {
+      onInit: function () {
+        // json binding for generic tiles
+        const oTilesModel = new JSONModel("../model/tiles.json");
+        this.getView().setModel(oTilesModel, "tiles");
+      },
 
-            }
-        });
+      onPress: function (sRoute) {
+        this.getOwnerComponent().getRouter().navTo(sRoute);
+      },
     });
+  }
+);
