a4ceba08daf6d314c54aadfb63b0d3f48f7d4d0e
alikapllan <alihkaplan44@gmail.com>
2024-12-05 21:26:02 +0100

    implement Delete Customer Page

--- tmui5/webapp/controller/DeleteCustomer.controller.js
+++ tmui5/webapp/controller/DeleteCustomer.controller.js
@@ -1,15 +1,27 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, History) {
+    "use strict";
 
-        return Controller.extend("tmui5.controller.DeleteCustomer", {
-            onInit: function () {
+    return Controller.extend("tmui5.controller.DeleteCustomer", {
+      onInit: function () {},
 
-            }
-        });
+      onDeleteSelectedCustomer: function () {},
+
+      onNavBack: function () {
+        const oHistory = History.getInstance();
+        const sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("RouteMainView", {}, true);
+        }
+      },
     });
+  }
+);
