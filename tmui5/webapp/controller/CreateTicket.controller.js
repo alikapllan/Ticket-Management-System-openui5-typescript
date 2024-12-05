14f6d70fe820062e4e57790c947da8c2e7d78ec3
alikapllan <alihkaplan44@gmail.com>
2024-12-05 01:23:49 +0100

    implement Create Ticket Page

--- tmui5/webapp/controller/CreateTicket.controller.js
+++ tmui5/webapp/controller/CreateTicket.controller.js
@@ -1,15 +1,31 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/ui/core/routing/History"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller, History) {
+    "use strict";
 
-        return Controller.extend("tmui5.controller.CreateTicket", {
-            onInit: function () {
+    return Controller.extend("tmui5.controller.CreateTicket", {
+      onInit: function () {
+        const oTicketNumberInput = this.byId("ticketNumberInput");
+        oTicketNumberInput.setValue("Dynamic Ticket Number will be here");
 
-            }
-        });
+        const oEmailInput = this.byId("emailInput");
+        oEmailInput.setValue("Email will be taken from 'assigned to'");
+      },
+
+      onNavBack: function () {
+        const oHistory = History.getInstance();
+        const sPreviousHash = oHistory.getPreviousHash();
+
+        if (sPreviousHash !== undefined) {
+          window.history.go(-1);
+        } else {
+          const oRouter = this.getOwnerComponent().getRouter();
+          oRouter.navTo("RouteMainView", {}, true);
+        }
+      },
     });
+  }
+);
