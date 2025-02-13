04ec679ecb0b61bc0ee1cb626d4eb9f560b00c4b
alikapllan <alihkaplan44@gmail.com>
2025-02-13 13:11:50 +0100

    App.controller.js to TypeScript
    ref: https://www.youtube.com/watch?v=24VmbFR8pqk&t=3s

--- tmui5/webapp/controller/App.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(["tmui5/controller/BaseController"], function (BaseController) {
-  "use strict";
-
-  return BaseController.extend("tmui5.controller.App", {
-    onInit: function () {},
-  });
-});
