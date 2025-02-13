4f4b0f8774bbdc0d4226bc47776ae6c8dbb222d9
alikapllan <alihkaplan44@gmail.com>
2025-02-13 13:07:01 +0100

    BaseController.js to TypeScript
    ref: https://www.youtube.com/watch?v=24VmbFR8pqk&t=3s

---
+++ tmui5/webapp/controller/BaseController.ts
@@ -0,0 +1,64 @@
+import Controller from "sap/ui/core/mvc/Controller";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
+import Component from "sap/ui/core/UIComponent";
+import JSONModel from "sap/ui/model/json/JSONModel";
+import MessageBox from "sap/m/MessageBox";
+import customerService from "tmui5/services/CustomerService";
+import teamMemberService from "tmui5/services/teamMemberService";
+import ticketService from "tmui5/services/ticketService";
+import Constants from "tmui5/constants/Constants";
+
+export default class BaseController extends Controller {
+  protected Constants: typeof Constants; // Explicit type
+  protected oBundle: any;
+
+  onInit(): void | undefined {
+    this.Constants = Constants; // Explicitly assign Constants
+
+    const oOwnerComponent = this.getOwnerComponent();
+    const oI18nModel = oOwnerComponent.getModel("i18n") as ResourceModel;
+
+    this.oBundle = oI18nModel.getResourceBundle();
+  }
+
+  private getRouter() {
+    return (this.getOwnerComponent() as Component).getRouter();
+  }
+
+  public navTo(sRoute: string): void {
+    this.getRouter().navTo(sRoute, {}, true);
+  }
+
+  public async loadTeamMembers(): Promise<void> {
+    try {
+      const teamMembers = await teamMemberService.fetchTeamMembers();
+      const oTeamMemberModel = new JSONModel(teamMembers);
+      this.getOwnerComponent().setModel(oTeamMemberModel, "teamMemberModel");
+    } catch (error) {
+      console.error(error);
+      MessageBox.error(this.oBundle.getText("MBoxGETReqFailedOnTeamMember"));
+    }
+  }
+
+  public async loadCustomers(): Promise<void> {
+    try {
+      const customers = await customerService.fetchCustomers();
+      const oCustomerModel = new JSONModel(customers);
+      this.getOwnerComponent().setModel(oCustomerModel, "customerModel");
+    } catch (error) {
+      console.error(error);
+      MessageBox.error(this.oBundle.getText("MBoxGETReqFailedOnCustomer"));
+    }
+  }
+
+  public async loadTickets(): Promise<void> {
+    try {
+      const tickets = await ticketService.fetchTickets();
+      const oTicketModel = new JSONModel(tickets);
+      this.getOwnerComponent().setModel(oTicketModel, "ticketModel");
+    } catch (error) {
+      console.error(error);
+      MessageBox.error(this.oBundle.getText("MBoxGETReqFailedOnTicket"));
+    }
+  }
+}
