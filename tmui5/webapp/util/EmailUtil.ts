90b9f3fa1099a4c05c5e911b76faca8af32b0442
alikapllan <alihkaplan44@gmail.com>
2025-02-20 11:13:34 +0100

    EmailUtil.js to Typescript

---
+++ tmui5/webapp/util/EmailUtil.ts
@@ -0,0 +1,30 @@
+import Integer from "sap/ui/model/type/Integer";
+import Constants from "tmui5/constants/Constants";
+import ticketEmailService from "tmui5/services/ticketEmailService";
+
+interface EmailPayload {
+  ticketId: Integer;
+  teamMemberEmail: string;
+  teamMemberFullName: string;
+  title: string;
+  description: string;
+}
+
+export const EmailUtil = {
+  sendEmail: async (
+    emailSendingType: string,
+    emailPayload: EmailPayload
+  ): Promise<void> => {
+    try {
+      if (emailSendingType === Constants.EMAIL_SENDING_TYPE.CREATED) {
+        await ticketEmailService.sendTicketCreatedEmail(emailPayload);
+      } else if (emailSendingType === Constants.EMAIL_SENDING_TYPE.UPDATED) {
+        await ticketEmailService.sendTicketUpdatedEmail(emailPayload);
+      }
+    } catch (error) {
+      console.error(`Failed to send ${emailSendingType} email:`, error);
+    }
+  },
+};
+
+export default EmailUtil;
