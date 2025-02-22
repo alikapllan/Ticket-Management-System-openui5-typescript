2692842eec315aab3f1803026bfd054a79381057
alikapllan <alihkaplan44@gmail.com>
2025-02-22 23:33:28 +0100

    Rest API Backend - controller - customerController.js to TypeScript

--- tmui5/backend-rest-api/controller/customerController.ts
+++ tmui5/backend-rest-api/controller/customerController.ts
@@ -1,7 +1,11 @@
-const pool = require("../config/db");
+import { Request, Response } from "express";
+import pool from "../config/db";
 
 // GET all customers
-const getAllCustomers = async (req, res) => {
+export const getAllCustomers = async (
+  _req: Request,
+  res: Response
+): Promise<void> => {
   console.log("Customer: Request GET");
 
   try {
@@ -17,46 +21,45 @@ const getAllCustomers = async (req, res) => {
       `
     );
     res.status(200).json(result.rows);
-  } catch (error) {
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 };
 
 // CREATE a customer
-const createCustomer = async (req, res) => {
+export const createCustomer = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const { name, email, phone } = req.body;
-
   console.log("Customer: Request body POST:", req.body);
 
   try {
     const result = await pool.query(
       `INSERT INTO "Customer" 
-        ("name", 
-         "email", 
-         "phone", 
-         "createdAt") 
+        ("name", "email", "phone", "createdAt") 
         VALUES ($1, $2, $3, NOW()::timestamp) 
         RETURNING *`,
       [name, email, phone]
     );
     res.status(201).json(result.rows[0]);
-  } catch (error) {
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 };
 
 // DELETE a customer
-const deleteCustomer = async (req, res) => {
+export const deleteCustomer = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
   const { id } = req.params;
-
   console.log("Customer: Request body DELETE, customerId: ", id);
 
   try {
     await pool.query('DELETE FROM "Customer" WHERE "customerId" = $1', [id]);
-    res.status(204).send(); // nocontent
-  } catch (error) {
+    res.status(204).send(); // No Content
+  } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
 };
-
-module.exports = { getAllCustomers, createCustomer, deleteCustomer };
