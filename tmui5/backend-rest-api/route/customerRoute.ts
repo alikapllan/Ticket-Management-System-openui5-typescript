84421da19769f4dafdbb0951ba845f255acc77e0
alikapllan <alihkaplan44@gmail.com>
2025-02-22 21:54:44 +0100

    Rest API Backend - route - customerRoute.js to TypeScript

---
+++ tmui5/backend-rest-api/route/customerRoute.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  getAllCustomers,
+  createCustomer,
+  deleteCustomer,
+} from "../controller/customerController";
+
+// router instance
+const router: Router = Router();
+
+// Define routes
+router.get("/", getAllCustomers);
+router.post("/", createCustomer);
+router.delete("/:id", deleteCustomer);
+
+export default router;
