c66b70f9a99c0be659b7195b73c1e9eee5fc7ba4
alikapllan <alihkaplan44@gmail.com>
2025-02-22 23:17:35 +0100

    Rest API Backend - route - ticketCommentRoute.js to TypeScript

---
+++ tmui5/backend-rest-api/route/ticketCommentRoute.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  getAllTicketComments,
+  createTicketComment,
+} from "../controller/ticketCommentController";
+
+const router: Router = Router();
+
+router.get("/:id", getAllTicketComments); // GET all ticket comments for a specific ticket
+router.post("/", createTicketComment);
+
+/* 
+    -> No need to define DELETE route:
+    PostgreSQL handles deletion automatically due to 'ON DELETE CASCADE' 
+    on the foreign key 'ticketId' in the 'TicketComment' table.
+    This ensures that when a ticket is deleted, all related comments 
+    are automatically removed by the database.
+*/
+
+// router.delete("/", deleteRelatedTicketComments);
+
+export default router;
